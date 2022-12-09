6f7998aec21c066993c56ede5d700819bfdd9c33
Kazuyuki FUKUDA <10156602+kzfk@users.noreply.github.com>
2022-12-09 18:12:41 +0900

    accessTokenConfig.json file, secrets folder

--- publish.sh
+++ publish.sh
@@ -2,7 +2,8 @@
 
 #sudo apt install -y osslsigncode
 
-rm -rf published
+## accessTokenConfig.json file, secrets folder
+#rm -rf published
 dotnet publish -c Release -o ./published src/SecureSign.Web/
 dotnet publish -c Release -o ./published src/SecureSign.Tools/
 cp appsettings.json published/
