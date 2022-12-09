eb4feb06b17932947706ea035ea059fda21d7339
Kazuyuki FUKUDA <10156602+kzfk@users.noreply.github.com>
2022-12-09 16:40:20 +0900

    publish script modify.

--- publish.sh
+++ publish.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
+#sudo apt install -y osslsigncode
+
 rm -rf published
-dotnet publish -c Release -o ../../published src/SecureSign.Web/
-dotnet publish -c Release -o ../../published src/SecureSign.Tools/
+dotnet publish -c Release -o ./published src/SecureSign.Web/
+dotnet publish -c Release -o ./published src/SecureSign.Tools/
 cp appsettings.json published/
