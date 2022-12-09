d7262e4a32928b1c5c7f3f605896eb5a20678364
Kazuyuki FUKUDA <10156602+kzfk@users.noreply.github.com>
2022-12-09 18:44:09 +0900

    protected generated files/folders

--- publish.sh
+++ publish.sh
@@ -2,8 +2,18 @@
 
 #sudo apt install -y osslsigncode
 
-## accessTokenConfig.json file, secrets folder
-#rm -rf published
+touch ./accessTokenConfig.json
+
+
+rm -rf published
 dotnet publish -c Release -o ./published src/SecureSign.Web/
 dotnet publish -c Release -o ./published src/SecureSign.Tools/
 cp appsettings.json published/
+
+## accessTokenConfig.json file, secrets folder, keys folder
+cd published/
+ln -s ../keys keys
+ln -s ../secrets secrets
+ln -s ../accessTokenConfig.json accessTokenConfig.json
+
+# dotnet SecureSign.Web.dll
